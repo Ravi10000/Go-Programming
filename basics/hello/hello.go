a3f184d6e7a8172508115b28e80e1f569b6c8797
Ravi Sharma <ruravi80@gmail.com>
2023-08-09 21:43:12 +0530

    add error on passing an empty string in Hello function

--- basics/hello/hello.go
+++ basics/hello/hello.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Ravi")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	message, error := greetings.Hello("")
+	if error != nil {
+		log.Fatal(error)
+	}
 	fmt.Println(message)
 }
